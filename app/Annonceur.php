a28d6d532bbb527913c65b59bf148045c33e150b
Gery Meli <meligery93@gmail.com>
2020-04-14 20:54:04 +0100

    Correction des buggs de pagination et de réinitialisation des formulaires.

--- app/Annonceur.php
+++ app/Annonceur.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonceur extends Model
 {
-    protected $fillable = ['nom', 'url', 'adresse_facturation', 'email_comptabilite', 'email_direction', 'email_production', 'delai_paiement', 'cree_par', 'modifie_par'];
+    protected $fillable = ['nom', 'login', 'password', 'url', 'adresse_facturation', 'email_comptabilite', 'email_direction', 'email_production', 'delai_paiement', 'cree_par', 'modifie_par'];
 
     public function campagnes()
     {
